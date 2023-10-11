5f4c61ccba96b904c0271028890b7091d3028102
sorisena <seenaabaayee@gmail.com>
2023-10-11 01:16:30 -0700

    0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Student module.
-
-Contains a Student class and some methods.
-"""
+"""This module defines a class Student"""
 
 
-class Student():
-    """Defines a Student."""
+class Student:
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Sets the necessary attributes for the Student object.
-
-        Args:
-            first_name (str): first name of the student.
-            last_name (str): last name of the student.
-            age (int): age of the student.
+        """Initializes a new Student
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance."""
-        if attrs is not None:
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        """Gets a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
